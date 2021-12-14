1c1c83bdbb0b1cdd67dfc077c1d83d610bc2e32d
Simone Guimaraes <simoneguimaraes11@gmail.com>
2021-12-14 10:05:45 -0300

    initial config auth

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div className="text-center">
+      <p>This is the homepage</p>
+      <div className="d-flex flex-column align-items-center">
+        <Link className="btn btn-lg btn-primary" to="/auth/signup">
+          Signup here!
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
